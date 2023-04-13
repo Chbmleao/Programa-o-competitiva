b644b9b3cfa803e2f7692f52292aed3fa00a815a
Carlos Leão <caikleao@gmail.com>
2023-04-13 00:12:50 -0300

    Cartesian plan problem from codeforces.

---
+++ ianVisitsMary.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    int numTests = 0;
+    cin >> numTests;
+
+    for (int i = 0; i < numTests; i++) {
+        int x, y;
+        cin >> x;
+        cin >> y;
+
+        
+        cout << "2" << endl;
+        cout << x-1 << " 1" << endl;
+        cout << x << " " << y << endl;
+        
+    }
+    
+
+
+    return 0;
+}
